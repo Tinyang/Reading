219228b59a2b78efadb833505863ae0a54580878
Tinyang <1251307117@qq.com>
2021-08-24 14:43:09 +0800

    Spring Boot start

---
+++ C_demo/cArray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+int balance[] = {10,20,30,40};
+
+int* addr1 = balance;
+int* addr2 = &balance[2];
+long addr3 = addr2 - addr1;
+
+printf("Value of *ip variable: %d\n", addr1 );
+printf("Value of *ip variable: %d\n", addr2 );
+printf("Value of *ip variable: %d\n", addr3 );
+
+//2293280
+//2293284
+//2293288
+
+
+}
